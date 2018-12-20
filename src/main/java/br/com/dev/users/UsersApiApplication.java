f1f26db08086e230c81c69f2de7f737a89a6adfd
Felipe Lima <flf.felipe@gmail.com>
2018-12-19 23:21:38 -0200

    Commit inicial

---
+++ src/main/java/br/com/dev/users/UsersApiApplication.java
@@ -0,0 +1,14 @@
+package br.com.dev.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersApiApplication.class, args);
+	}
+
+}
+
